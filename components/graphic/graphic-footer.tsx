d97c215a49cd4a62133e46cd4b87a7520e6641c0
DeveloperKhan <adib.khan@gmail.com>
2026-02-08 01:13:46 -0800

    Fix website icon

--- components/graphic/graphic-footer.tsx
+++ components/graphic/graphic-footer.tsx
@@ -45,16 +45,8 @@ export function GraphicFooter() {
 
       {/* Website */}
       <div style={{ display: "flex", alignItems: "center", gap: 8, color: "white", fontSize: 30 }}>
-        <svg
-          width={ICON_SIZE}
-          height={ICON_SIZE}
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="white"
-          strokeWidth="2"
-        >
-          <circle cx="12" cy="12" r="10" />
-          <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+        <svg width={ICON_SIZE} height={ICON_SIZE} viewBox="0 0 24 24" fill="white">
+          <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
         </svg>
         <span>dracoviz.com</span>
       </div>
